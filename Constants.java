0484176c61c3e96974177d172245e76764886d7b
Isabel Dominik <icd107@live.com>
2018-11-17 10:42:40 -0800

    Added dining hall constant names

--- Constants.java
+++ Constants.java
@@ -1,3 +1,11 @@
-public class Constants {
-	
+public class Constants
+{
+	public static final String COVEL_NAME = "Covel";
+	public static final String DENEVE_NAME = "De Neve";
+	public static final String BPLATE_NAME = "Bruin Plate";
+	public static final String FEAST_NAME = "Feast";
+	public static final String RENDE_NAME = "Rendevous";
+	public static final String CAFE1919_NAME = "Cafe 1919";
+	public static final String BCAFE_NAME = "Bruin Cafe";
+	public static final String STUDY_NAME = "The Study at Hedrick";
 }
