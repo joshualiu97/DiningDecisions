e2e27162c5ac1efb90b169aedb3a11cc6e9cbb11
Isabel Dominik <icd107@live.com>
2018-11-17 10:51:12 -0800

    Added more constants

--- Constants.java
+++ Constants.java
@@ -1,11 +1,36 @@
 public class Constants
 {
-	public static final String COVEL_NAME = "Covel";
-	public static final String DENEVE_NAME = "De Neve";
-	public static final String BPLATE_NAME = "Bruin Plate";
-	public static final String FEAST_NAME = "Feast";
-	public static final String RENDE_NAME = "Rendevous";
-	public static final String CAFE1919_NAME = "Cafe 1919";
-	public static final String BCAFE_NAME = "Bruin Cafe";
-	public static final String STUDY_NAME = "The Study at Hedrick";
+	//Dining hall names
+	public static final String COVEL_NAME 		= "Covel";
+	public static final String DENEVE_NAME		= "De Neve";
+	public static final String BPLATE_NAME 		= "Bruin Plate";
+	public static final String FEAST_NAME 		= "Feast";
+	public static final String RENDE_NAME 		= "Rendevous";
+	public static final String CAFE19_NAME 		= "Cafe 1919";
+	public static final String BCAFE_NAME 		= "Bruin Cafe";
+	public static final String STUDY_NAME 		= "The Study at Hedrick";
+
+	//Health levels
+	//Lower number = unhealthy, higher unmber = healthy
+	public static final int COVEL_HEALTH 		= 0;
+	public static final int DENEVE_HEALTH 		= 1;
+	public static final int BPLATE_HEALTH 		= 8;
+	public static final int FEAST_HEALTH 		= 5;
+	public static final int RENDE_HEALTH 		= 3;
+	public static final int CAFE19_HEALTH 		= 4;
+	public static final int BCAFE_HEALTH 		= 6;
+	public static final int STUDY_HEALTH 		= 7;
+
+	//Takeout or not
+	//true = is takeout, false = not takeout
+	public static final boolean COVEL_TAKEOUT	= false;
+	public static final boolean DENEVE_TAKEOUT 	= false;
+	public static final boolean BPLATE_TAKEOUT 	= false;
+	public static final boolean FEAST_TAKEOUT 	= false;
+	public static final boolean RENDE_TAKEOUT	= true;
+	public static final boolean CAFE19_TAKEOUT 	= true;
+	public static final boolean BCAFE_TAKEOUT 	= true;
+	public static final boolean STUDY_TAKEOUT 	= true;
+
+	//Mood
 }
