85643dc1615c26b085465a3176b56de292510488
Grace <lollypop631@gmail.com>
2018-11-17 12:41:44 -0800

    Add points to DiningHall

--- DiningHall.java
+++ DiningHall.java
@@ -2,10 +2,12 @@ public class DiningHall {
 
 	private double crowdLevel;
 	private boolean isOpen;
+	private int points;
 
 	public DiningHall(int crowdLevel, boolean isOpen){
 		this.crowdLevel = crowdLevel;			
 		this.isOpen = isOpen;
+		this.points = 0;
 	}
 
 	public double getcrowdLevel(){
@@ -14,5 +16,14 @@ public class DiningHall {
 
 	public boolean getisOpen(){
 		return isOpen;
+	}
+
+	public int getpoints(){
+		return points;
+	}
+
+	public int addPoints(int add){
+		points += add;
+
 	}
 }
