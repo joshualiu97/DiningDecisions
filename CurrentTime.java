6d03e6ea9003e0c3e69b050cee01771f308d6f1a
Grace <lollypop631@gmail.com>
2018-11-17 13:17:05 -0800

    Updated CurrentTime to have better methods

--- CurrentTime.java
+++ CurrentTime.java
@@ -1,15 +1,21 @@
 import java.util.Date;
 
 public class CurrentTime {
+	private int currentTime;
 
-    public String getCurrentTime(){
+	public class CurrentTime(int currentTime){
+		this.currentTime() = retrieveCurrentTime(); 
 
+	}
+
+    public String retrieveCurrentTime(){
         Date date = new Date();
-
         String time = date.toString();
-    
         String t1 = time.substring(11,13) + time.substring(14,16);
-
         return t1;
+   }
+
+   public String geturrentTime(){
+   		return currentTime();
    }
 }
