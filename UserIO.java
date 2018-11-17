8a0aca947572bfa20d2d6e3fdd3b1adc74efa31e
Melody Chen <melodyc18113765@tas.tw>
2018-11-17 11:08:22 -0800

    added constructor

--- UserIO.java
+++ UserIO.java
@@ -1,3 +1,21 @@
 public class UserIO {
+	private String mood_Question;
+	private String health_Question; 
+	private String loc_Question; 
+	private String hunger_Question; 
+	private String takeOut_Question; 
+	public UserIO()
+	{
+		mood_Question = "What mood are you in right now?";
+		health_Question = "How healthy are you feeling right now?";
+		loc_Question = "Where are you closest to right now?";
+		hunger_Question = "How hungry are you feeling right now?";
+		takeOut_Question = "Are you fine with eating at takeout places?";
+	}
+
+	public void askQuestions()
+	{
+
+	}
 	
 }
