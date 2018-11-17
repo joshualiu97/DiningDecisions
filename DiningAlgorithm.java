a6937e101d146657c4f96dacc52190292a8e1171
Isabel Dominik <icd107@live.com>
2018-11-17 11:08:18 -0800

    Added local variables to DiningAlgorithm

---
+++ DiningAlgorithm.java
@@ -0,0 +1,30 @@
+import "Constants.java";
+import "UserIO.java";
+
+public class DiningAlgorithm
+{
+	private int userMood;
+	private int userHealthLevel;
+	private int location;
+	private int hunger;
+	private int takeOut;
+
+	public DiningAlgorithm()
+	{
+		userMood = 0;
+		userHealthLevel = 0;
+		location = 0;
+		hunger = 0;
+		takeOut = 0;
+	}
+
+	public void getData()
+	{
+		userMood = 
+	}
+
+	public String calculateDiningHall()
+	{
+
+	}
+}
